c69622f2fd03a53a66665d825f78cb5dd7b58dbd
adam <adam.overstreet@gmail.com>
2013-07-19 11:31:27 -0400

    adding homework

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,10 @@
 class Movie < ActiveRecord::Base
 
 	#validation
-	validates :title, :description, :year_released, :ratings presence: true
+	validates :title, :description, :year_released, :ratings, presence: true
 	validates :title, :description, length: {minimum: 10} 
-	
+	validates_numericality_of :year_released, :only_integer => true, :greater_than => 0
+	validates_numericality_of :ratings, :only_integer => true, :greater_than_or_equal_to => 1, :less_than => 6
 	
 	
 	def self.search_for(query)
